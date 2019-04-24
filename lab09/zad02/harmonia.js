79927f3db39c61a36cdf0bcd1fb9b965de20b60e
Mateusz Soroka <xmsoroka@gmail.com>
2019-04-24 18:21:50 +0200

    zad2, zad3 done

---
+++ lab09/zad02/harmonia.js
@@ -0,0 +1,20 @@
+/* jshint strict: global, esversion: 6, devel: true, node: true, browser:true */
+'use strict';
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    let headers = document.querySelectorAll('.hd');
+
+    Array.from(headers).forEach((header) => {
+        header.addEventListener('click', (event) => {
+            event.preventDefault();
+            
+            if(header.nextSibling.nextSibling.style.display == 'none') {
+                header.nextSibling.nextSibling.style.display = 'block';
+            } else {
+                header.nextSibling.nextSibling.style.display = 'none';
+            }
+        });
+    });
+
+});
---
+++ lab09/zad03/harmonia.js
@@ -0,0 +1,23 @@
+/* jshint strict: global, esversion: 6, devel: true, node: true, browser:true */
+'use strict';
+
+document.addEventListener("DOMContentLoaded", function () {
+    let headers = document.querySelectorAll('.hd');
+
+    Array.from(headers).forEach((header) => {
+        header.nextSibling.nextSibling.style.display = 'none';
+
+        header.addEventListener('mouseout', (event) => {
+            event.preventDefault();
+
+            header.nextSibling.nextSibling.style.display = 'none';
+        });
+
+        header.addEventListener('mouseover', (event) => {
+            event.preventDefault();
+            
+            header.nextSibling.nextSibling.style.display = 'block';
+        });
+    });
+
+});
