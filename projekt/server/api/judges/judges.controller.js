e6e935c16121c7fe22bd5b3770813cec1658ded9
Mateusz Soroka <xmsoroka@gmail.com>
2019-06-01 13:20:02 +0200

    Judges CRUD

---
+++ projekt/server/api/judges/judges.controller.js
@@ -0,0 +1,72 @@
+var Judges = require('./judges.dao');
+
+exports.createJudge = function (req, res, next) {
+    var judge = {
+        sedzia: req.body.sedzia,
+        kraj: req.body.kraj
+    };
+
+    Judges.create(judge, function (err, judge) {
+        if (err) {
+            res.json({
+                error: err
+            })
+        }
+        res.json({
+            message: "Judge created successfully"
+        })
+    })
+};
+
+exports.getJudge = function (req, res, next) {
+    Judges.get({_id: req.params.id}, function (err, judge) {
+        if (err) {
+            res.json({
+                error: err
+            })
+        }
+        res.send(judge);
+    })
+};
+
+exports.getJudges = function (req, res, next) {
+    Judges.get({}, function (err, judges) {
+        if (err) {
+            res.json({
+                error: err
+            })
+        }
+        res.send(judges);
+    })
+};
+
+exports.updateJudge = function (req, res, next) {
+    var judge = {
+        sedzia: req.body.sedzia,
+        kraj: req.body.kraj
+    }
+
+    Judges.update({_id: req.params.id}, judge, function (err, judge) {
+        if (err) {
+            res.json({
+                error: err
+            })
+        }
+        res.json({
+            message: "Judge updated successfully"
+        })
+    })
+}
+
+exports.removeJudge = function (req, res, next) {
+    Judges.delete({_id: req.params.id}, function (err, judge) {
+        if (err) {
+            res.json({
+                error: err
+            })
+        }
+        res.json({
+            message: "Judge deleted successfully"
+        })
+    })
+}
---
+++ projekt/server/app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var log = require('morgan')('dev');
+var bodyParser = require('body-parser');
+
+var properties = require('./config/properties');
+var db = require('./config/database');
+var judgesRoutes = require('./api/judges/judges.route');
+var app = express();
+
+var bodyParserJSON = bodyParser.json();
+var bodyParserURLEncoded = bodyParser.urlencoded({extended: true});
+var router = express.Router();
+
+db();
+
+app.use(log);
+app.use(bodyParserJSON);
+app.use(bodyParserURLEncoded);
+
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization");
+    next();
+});
+
+
+app.use('/', router);
+judgesRoutes(router);
+
+app.listen(properties.PORT, (req, res) => {
+    console.log(`Server działa na porcie ${properties.PORT}`);
+});
